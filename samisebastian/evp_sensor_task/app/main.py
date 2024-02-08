0cdf8e4ca75a95f8f1466a0f7743b6d24e5816e6
RetepBassetian <115300450+Retep-Bassetian@users.noreply.github.com>
2024-02-08 17:51:43 +0100

    Update main.py

--- samisebastian/evp_sensor_task/app/main.py
+++ samisebastian/evp_sensor_task/app/main.py
@@ -1,24 +1,23 @@
 # coding=utf-8
-# Hier importieren wir das Flask-Framework 'flask', sowie 'jsonify' und 'render_template'
+# Importieren der benötigten Module: Flask, jsonify, render_template, mysql.connector, signal, sys und CORS
 from flask import Flask, jsonify, render_template
 import mysql.connector
 import signal
 import sys
 from flask_cors import CORS
 
-
+# Definitieren einer Funktion zum Schließen der MySQL-Verbindung bei Programmende
 def close_connection(signal, frame):
-    # Close MySQL connection
+    # Schließe die MySQL-Verbindung
     db.close()
     print("MySQL connection closed.")
     sys.exit(0)
 
-
-# Register signal handler for shutdown
+# Registrieren des Signalhandlers für das Beenden des Programms
 signal.signal(signal.SIGINT, close_connection)
 signal.signal(signal.SIGTERM, close_connection)
 
-
+# Definieren einer Datenklasse DataObject zur Strukturierung der Daten
 class DataObject:
     def __init__(self, id, temperature, humidity, co2, dust):
         self.id = id
@@ -27,12 +26,12 @@ class DataObject:
         self.co2 = co2
         self.dust = dust
 
-
-# Hier wird die Flask-Anwendung instanziiert
+# Instanziieren der Flask-Anwendung
 app = Flask(__name__)
+# Aktivieren von CORS (Cross-Origin Resource Sharing) für die Flask-Anwendung
 CORS(app)
 
-# Creating a connection object
+# Herstellen einer Verbindung zur MySQL-Datenbank
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -40,43 +39,38 @@ db = mysql.connector.connect(
     database="sensor_data"
 )
 
+# Erstellen eines Cursor-Objekts für die Datenbankverbindung
 cursor = db.cursor()
 
-# Definiere ersten API-Endpunkt:
-# Wenn per GET Methode aufgerufen
-
-
+# Definieren des ersten API-Endpunkts: '/api/data'
+# Zugriffsmethode: GET
 @app.route('/api/data', methods=['GET'])
-# Wird get_data() ausgeführt
+# Ausführen der Funktion get_data()
 def get_data():
-    # Hier werden die zurückgegebenen Daten festgelegt
+    # Festlegen der SQL-Abfrage
     query = "SELECT color FROM data ORDER BY timeIndex LIMIT 1"
-    # Execute the query
+    # Ausführen der SQL-Abfrage
     cursor.execute(query)
-
-    # Fetch all the rows returned by the query
+    # Abrufen aller zurückgegebenen Zeilen
     rows = cursor.fetchall()
-
-    # Create a list of DataObject instances
+    # Erstellen einer Liste von DataObject-Instanzen
     data_objects = []
-
-    # Iterate over the rows and create DataObject instances
+    # Über die zurückgegebenen Zeilen iterieren und Erstellen von DataObject-Instanzen
     for row in rows:
         data_objects.append(row)
-
+    # Ausgebeben der ersten DataObject-Instanz
     print(data_objects[0])
+    # Rückgabe der Daten im JSON-Format
     return jsonify(data_objects)
 
-# Definiere zweiten API-Endpunkt, die 'Hauptseite'
-
-
+# Definieren des zweiten API-Endpunkts: '/'
+# Zugriffsmethode: GET
 @app.route('/')
-# Wird index() ausgeführt
+# Ausführen der Funktion index()
 def index():
-    # Gibt eine HTML-Vorlage zurück
+    # Rückgabe einer HTML-Vorlage
     return render_template('index.html')
 
-
-# Starten der Anwendung
+# Starten der Flask-Anwendung
 if __name__ == '__main__':
     app.run()
